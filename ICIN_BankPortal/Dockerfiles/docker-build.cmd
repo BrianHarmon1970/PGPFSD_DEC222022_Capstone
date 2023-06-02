7b21f9422bdb0c93161da434edcbadcd6b246f78
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2023-06-02 14:38:16 -0500

    Bank Portal web server integrated into Docker project

--- ICIN_BankPortal/Dockerfiles/docker-build.cmd
+++ ICIN_BankPortal/Dockerfiles/docker-build.cmd
@@ -1,2 +1,3 @@
 docker build -t brianharmon1970/icin_database:0.0.1 -f Dockerfile-mysql ../ICIN_BankService
 docker build -t brianharmon1970/icin_service:0.0.1 -f Dockerfile-ubuntu ../ICIN_BankService
+docker build -t brianharmon1970/icin_webserver:0.0.1 -f Dockerfile-WebServer ../ICINBankWebFront
